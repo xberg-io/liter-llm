ce02f237250d87d03d033e09f841de3c6619ce77
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-05-27 12:27:13 +0200

    chore: update Elixir NIF checksums for v1.4.0-rc.35

--- packages/elixir/checksum-Elixir.LiterLlm.Native.exs
+++ packages/elixir/checksum-Elixir.LiterLlm.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.16-aarch64-apple-darwin.dylib.tar.gz" => "sha256:44952346892b2faa6740c7f59ab76dae3774b78fefe7cdd6693949caf4603673",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:4c1c46e9e46dccfeeb11532a8b751dc3e52c7c39ba17c44e5c39647009087f69",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:c94ca38a5845238a4b6705d8c3bfa601703a4c1b7ce285c6c4e1db8abe210229",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:26769859bfa5e8a93a1a1c9700a3492cf3263cf4a813fa866808cce6536cedd3",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.17-aarch64-apple-darwin.dylib.tar.gz" => "sha256:624dd22450268a46c2e496d8a44992459903df979be0cf3e0fbd864f40f3cf7e",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b43b7a4cdaece3c73ba3c98c508bce879979690117a5d80180daae011d8a1c5d",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.17-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:c0f0c188bd503a5df40f3b5927fcf9e58c0d3b55627c24dca1dce17ed35e8b96",
-  "libliter_llm_nif-v1.4.0-rc.33-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:6bc0d71c3d4c22f1492253b31e3a1105052a9d95548d8d6bc009cbd10d480397",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.16-aarch64-apple-darwin.dylib.tar.gz" => "sha256:bc1a15afdda64caeada46190e3d198e2d198f4a8b30a58617f68b05a967d6e61",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:81630806f7715f481491c6d4e5c0ffe55bcc663aaae05c0ec7fae8a4fdcf70eb",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:3b3a2762c4ed8856fda8757a964c5899b2dbc5970163a4d73753318d39de71bc",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:fea563d8aa9540f29d250ffa25687e61f95c9c349f8227a80edda9f868948005",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.17-aarch64-apple-darwin.dylib.tar.gz" => "sha256:9c0964e1e267b68b87be7b1fa4abf5f6c3af1422afcead90bfe0f6a7b6bbdb00",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:81630806f7715f481491c6d4e5c0ffe55bcc663aaae05c0ec7fae8a4fdcf70eb",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.17-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:1e1cd9e1eb2d407d5f114c5689da6427ed7f124885f571d3ff75bd8c230c2f43",
+  "libliter_llm_nif-v1.4.0-rc.35-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3ddfcd019efbe91d672bba9e69b9f22740567b71936c1a6c44e4cd1216600c39",
 }
