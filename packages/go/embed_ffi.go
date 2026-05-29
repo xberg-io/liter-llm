2a7edc913b2bdbd5e121ac4792f99a4e917fc7b6
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-05-29 14:09:06 +0200

    chore: bump alef to v0.20.12

--- packages/go/embed_ffi.go
+++ packages/go/embed_ffi.go
@@ -3,7 +3,7 @@
 
 package literllm
 
-import _ "embed"
+import "embed"
 
 // This file ensures that FFI header files and library artifacts are included
 // when this module is vendored. The //go:embed directive tells Go to include
